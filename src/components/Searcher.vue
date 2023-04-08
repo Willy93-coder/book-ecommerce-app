09587892cc5938af7ee4569a49a5644e509698c9
Willy <guillecg13@gmail.com>
2023-04-08 17:07:49 +0200

    feat: created searcher component without functionality

---
+++ src/components/Searcher.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-text-field 
+        clearable
+        variant="outlined" 
+        placeholder="Buscador..." 
+        density="compact" 
+        prepend-inner-icon="search" 
+        single-line 
+        hide-details>
+    </v-text-field>
+</template>
+
+<script>
+    export default {
+        data:() => {
+            
+        }
+    }
+</script>
