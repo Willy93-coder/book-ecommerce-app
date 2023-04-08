845b531ce92809118db79f9176f82dfed333822f
Willy <guillecg13@gmail.com>
2023-04-08 17:06:27 +0200

    feat: created dropdown user component after login, 3 options: profile, change password or log out

---
+++ src/components/UserDropdownIn.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-menu
+        open-on-hover
+    >
+        <template v-slot:activator="{ props }">
+            <v-btn
+                variant="plain"
+                v-bind="props"
+                icon="person" 
+            />
+        </template>
+
+        <v-list variant="plain">
+            <v-list-item
+                v-for="(item, index) in items"
+                :key="index"
+            >
+                <v-btn class="text-capitalize" variant="plain">{{ item.title }}</v-btn>
+            </v-list-item>
+        </v-list>
+    </v-menu>
+</template>
+
+<script>
+    export default {
+        data:() => ({
+            items: [
+                {title: "Perfil"},
+                {title: "Cambiar Contraseña"},
+                {title: "Cerrar sesión"},
+            ]
+        })
+    }
+</script>
