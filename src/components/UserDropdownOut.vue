c7ead3d505a03635d490d6317cec786ce85d1fc1
Willy <guillecg13@gmail.com>
2023-04-08 17:04:35 +0200

    feat: created dropdown user component before login, 2 options: login or register

---
+++ src/components/UserDropdownOut.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-menu
+        open-on-hover
+    >
+        <template v-slot:activator="{ props }">
+            <v-btn
+                variant="plain"
+                v-bind="props"
+                icon="person" 
+            />
+        </template>
+
+        <v-list variant="plain">
+            <v-list-item
+                v-for="(item, index) in items"
+                :key="index"
+            >
+                <v-btn class="text-capitalize" variant="plain">{{ item.title }}</v-btn>
+            </v-list-item>
+        </v-list>
+    </v-menu>
+</template>
+
+<script>
+    export default {
+        data:() => ({
+            items: [
+                {title: "Iniciar sesión"},
+                {title: "Registrarse"},
+            ]
+        })
+    }
+</script>
