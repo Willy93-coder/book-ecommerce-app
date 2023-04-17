225d0ca2f95655bbaaee0259542c590ff63ceb5b
Dimitar <dimitar.iliev42@gmail.com>
2023-04-17 19:48:37 +0200

    Add: Register rooting

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -29,7 +29,7 @@ export default {
     },
     methods: {
         registrarse() {
-            fetch('/registro', {
+            fetch('/registration', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
